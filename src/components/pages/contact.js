82d1523b5d48cf620135840233cbf594418c5c7c
Dhvanit Merchant <200382214@student.georgianc.on.ca>
2019-07-29 17:53:47 -0400

    first commit

---
+++ src/components/pages/contact.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Contact() {
+  return (
+<div class = "container">
+<div class="jumbotron text-center">
+<h1>Conatct hustlers</h1>
+</div>
+<div class="row">
+<div class="card">
+<img src="./images/dm.jpg" />
+
+<h4>Dhvanit Merchant</h4>
+<p>SN# 200382214</p>
+
+</div>
+
+<div class="card">
+<img src="./images/ms.jpeg" />
+
+<h4>Mehul Shah</h4>
+<p>SN# 200390083</p>
+
+</div>
+
+<div class="card">
+<img src="./images/dp.jpeg" />
+
+<h4>Dhruv Patel</h4>
+<p>SN# 200382210</p>
+
+</div>
+
+
+<div class="card">
+<img src="./images/js.jpeg" />
+
+<h4>Jemin Suvagiya</h4>
+<p>SN# 200372988</p>
+
+</div>
+
+</div>
+
+
+</div>
+  
+
+
+);
+}
+
+export default Contact;
